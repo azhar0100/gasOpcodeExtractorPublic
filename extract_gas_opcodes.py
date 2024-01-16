1d0728435be82443dfc101b22ad12b7df383aaef
azhar0100 <azhartahir0100@gmail.com>
2024-01-17 03:31:04 +0500

    Adde new processing

--- extract_gas_opcodes.py
+++ extract_gas_opcodes.py
@@ -1,7 +1,12 @@
+from pprint import pprint
 import requests
 import re
+from joblib import Memory
 
-def extract_gas_opcodes(url="https://raw.githubusercontent.com/Shirk/vim-gas/master/syntax/gas.vim"):
+mem = Memory('gasCodeMem')
+
+@mem.cache
+def download_opcode_file(url="https://raw.githubusercontent.com/Shirk/vim-gas/master/syntax/gas.vim"):
     # Download the file content
     response = requests.get(url)
     if response.status_code != 200:
@@ -9,5 +14,18 @@ def extract_gas_opcodes(url="https://raw.githubusercontent.com/Shirk/vim-gas/mas
 
     return response.text
 
-opcodes = extract_gas_opcodes()
-print(opcodes)
+opcode_text = download_opcode_file()
+opcode_lines = opcode_text.split('\n')
+
+opcode_lines = [x for x in opcode_lines if x.startswith('syn keyword')]
+# print("\n".join(opcode_lines))
+opcode_lines_stripped_of_begin_words = [x[len('syn keyword gasOpcode_'):] for x in opcode_lines]
+opcode_lines_with_instruction_code = [(x.split()[0],x.split()[1:]) for x in opcode_lines_stripped_of_begin_words]
+dict_opcode_list = {}
+for k,vs in opcode_lines_with_instruction_code:
+    dict_opcode_list.setdefault(k,[])
+    dict_opcode_list[k] += vs
+
+dict_opcode_list = {k:set(vs) for k,vs in dict_opcode_list.items()}
+wi
+pprint(dict_opcode_list)
