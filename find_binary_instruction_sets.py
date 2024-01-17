0211f0336373e9d1150bfbc399de415ac40eacf2
azhar0100 <azhartahir0100@gmail.com>
2024-01-17 05:02:22 +0500

    Added script version

--- find_binary_instruction_sets.py
+++ find_binary_instruction_sets.py
@@ -1,17 +1,6 @@
 import json
+import sys
 from pprint import pprint
-
-file_path = "/home/azhar/gasOpcodeExtractor/gas_codes.json"
-
-with open(file_path, "r") as file:
-    data = json.load(file)
-
-faster_lookup_data = {x:k for k,v in data.items() for x in v}
-
-dumped_obj_file = '/home/azhar/gasOpcodeExtractor/dumpedobj.txt'
-
-with open(dumped_obj_file, "r") as file:
-    objdumped_data = file.readlines()
 
 def separate_dumped_data_sections(objdumped_data):
     sections = []
@@ -32,14 +21,30 @@ def find_instruction_lines_in_section(section):
             instructions.append(line.split('\t')[-1].strip())
     return instructions
 
-def identify_all_instruction_sets_in_instruction_line_using_faster_lookup_data(instruction_line):
+def identify_all_instruction_sets_in_instruction_line_using_faster_lookup_data(instruction_line, faster_lookup_data):
     instruction_sets = []
     for instruction in instruction_line.split():
         if instruction in faster_lookup_data:
             instruction_sets.append(faster_lookup_data[instruction])
     return sorted(list(set(instruction_sets)))
 
-extracted_instruction_set_data = [[identify_all_instruction_sets_in_instruction_line_using_faster_lookup_data(x) for x in find_instruction_lines_in_section(section)] for section in separate_dumped_data_sections(objdumped_data)]
-extracted_instruction_set_data_flattened = sorted(list(set([x1 for x3 in extracted_instruction_set_data for x2 in x3 for x1 in x2])))
-pprint(extracted_instruction_set_data_flattened)
+def main(file_path, dumped_obj_file):
+    with open(file_path, "r") as file:
+        data = json.load(file)
 
+    faster_lookup_data = {x:k for k,v in data.items() for x in v}
+
+    with open(dumped_obj_file, "r") as file:
+        objdumped_data = file.readlines()
+
+    extracted_instruction_set_data = [[identify_all_instruction_sets_in_instruction_line_using_faster_lookup_data(x, faster_lookup_data) for x in find_instruction_lines_in_section(section)] for section in separate_dumped_data_sections(objdumped_data)]
+    extracted_instruction_set_data_flattened = sorted(list(set([x1 for x3 in extracted_instruction_set_data for x2 in x3 for x1 in x2])))
+    pprint(extracted_instruction_set_data_flattened)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python find_binary_instruction_sets.py <file_path> <dumped_obj_file>")
+    else:
+        file_path = sys.argv[1]
+        dumped_obj_file = sys.argv[2]
+        main(file_path, dumped_obj_file)
